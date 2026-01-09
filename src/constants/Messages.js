dc5445620039e3d44516dca1888e248088168dc2
pinetree5115-collab <pinetree5115@gmail.com>
2026-01-10 05:01:12 +0900

    docs: [README.md]초기 작성 및 기능 목록 정의

--- src/constants/Messages.js
+++
@@ -1,8 +0,0 @@
-export const INPUT_MESSAGES = {
-  PURCHASE_AMOUNT: "구입금액을 입력해 주세요.\n",
-};
-
-export const ERROR_MESSAGES = {
-  NOT_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
-  INVALID_UNIT: "[ERROR] 1,000원 단위로 입력해 주세요.",
-};
